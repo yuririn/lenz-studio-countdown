03f599b265e972363b8709f75b366819b5756d4e
yuririn <yuri.soushi@gmail.com>
2021-05-24 21:03:14 +0800

    追加

--- lenz-studio-countdown.php
+++ lenz-studio-countdown.php
@@ -3,7 +3,7 @@
  * Plugin Name: LZ Count down timer
  * Description: カウントダウンタイマーです。ショートコードで追加できます。
  * Author: 銀ねこアトリエ
- * Version: 1.0
+ * Version: 1.1
  * Author URI: https://ginneko-atelier.com
  *
  * @package Count Down Timer
